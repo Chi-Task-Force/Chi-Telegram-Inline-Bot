afb372824d704583699c08df11d8b0dfb1cf89e2
PhotonQuantum <self@lightquantum.me>
2020-12-19 19:36:52 +0800

    feat: stat report

--- chi_tg_inline_bot/__main__.py
+++ chi_tg_inline_bot/__main__.py
@@ -1,9 +1,10 @@
 import hashlib
 import logging
 import os
-from typing import List
+from queue import Queue
+from typing import List, Set
 
-from aiogram import Bot, Dispatcher, executor
+from aiogram import Bot, Dispatcher, executor, types
 from aiogram.types import ChosenInlineResult, InlineQuery, InlineQueryResultArticle, InputTextMessageContent
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from httpx import AsyncClient
@@ -19,6 +20,7 @@ bot = Bot(token=API_TOKEN, proxy=PROXY)
 dp = Dispatcher(bot)
 
 answer_map = {}
+stat_set: Set[str] = set()
 
 
 async def update_corpus():
@@ -35,6 +37,11 @@ async def inline_echo(inline_query: InlineQuery):
     text = inline_query.query
     answers: List[str] = seller.sell(text)
     moan: str = seller.vegetable_moan()
+    sell_count = await get_user_stat(inline_query.from_user.id)
+    sell_stat: str = f"我已经卖了 {sell_count} 句菜{'，我 zc' if sell_count > 20 else ''}"
+
+    stat_hash: str = hashlib.md5(sell_stat.encode()).hexdigest()
+    stat_set.add(stat_hash)
 
     answers_hash = {answer: hashlib.md5(answer.encode()).hexdigest() for answer in answers}
     for answer, _hash in answers_hash.items():
@@ -47,15 +54,34 @@ async def inline_echo(inline_query: InlineQuery):
                 id=_hash,
                 title=answer,
                 input_message_content=InputTextMessageContent(answer)
-            ) for answer, _hash in answers_hash.items()]
+            ) for answer, _hash in answers_hash.items()] + \
+            [InlineQueryResultArticle(id=stat_hash,
+                                      title="卖菜统计",
+                                      input_message_content=InputTextMessageContent(sell_stat))]
 
     await bot.answer_inline_query(inline_query.id, results=items, cache_time=0, is_personal=True)
 
 
+async def get_user_stat(user_id: int) -> int:
+    user_id = hashlib.md5(user_id.to_bytes(16, "little", signed=False)).hexdigest()
+    return await logger.get_user_stat(user_id)
+
+
 @dp.chosen_inline_handler()
 async def inline_choice(result: ChosenInlineResult):
-    await logger.log(answer_map.get(result.result_id, -1),
+    if result.result_id in stat_set:
+        stat_set.remove(result.result_id)
+        return
+    await logger.log(answer_map.get(result.result_id, "-1"),
                      hashlib.md5(result.from_user.id.to_bytes(16, "little", signed=False)).hexdigest())
+
+
+@dp.message_handler(commands=["stat"])
+async def stat_handler(message: types.Message):
+    stat = await logger.get_stat()
+    top_sentences = "\n".join([f"{k}：{v} 次" for k, v in stat.top_sentences.items()])
+    await message.answer(f"总共已经有 {stat.users} 名迟化人卖了 {stat.total_requests} 句菜\n其中最迟的人卖了 {stat.top_user_count} 句\n\n" \
+                         f"被卖得最多次的句子：\n{top_sentences}")
 
 
 logger = InlineLogger(os.environ["LOG_FILE"])
--- chi_tg_inline_bot/logger.py
+++ chi_tg_inline_bot/logger.py
@@ -1,9 +1,17 @@
 import json
 import os
+from dataclasses import dataclass
 from asyncio import Lock
+from typing import Dict, Optional
 
 import aiofiles
 
+@dataclass
+class Stat:
+    total_requests: int
+    users: int
+    top_sentences: Dict[str, int]
+    top_user_count: int
 
 class InlineLogger:
     def __init__(self, filename: str):
@@ -19,11 +27,18 @@ class InlineLogger:
         if not self.data:
             self.data = {"total": 0, "per_sentence": {}, "per_user": {}}
 
-
-    async def log(self, sentence, user):
+    async def log(self, sentence: str, user: str):
         async with self.lock:
             self.data["total"] += 1
             self.data["per_sentence"][sentence] = self.data["per_sentence"].get(sentence, 0) + 1
             self.data["per_user"][user] = self.data["per_user"].get(user, 0) + 1
             async with aiofiles.open(self.filename, mode="w") as f:
                 await f.write(json.dumps(self.data, ensure_ascii=False))
+
+    async def get_user_stat(self, user: str) -> Optional[int]:
+        return self.data["per_user"].get(user)
+
+    async def get_stat(self) -> Stat:
+        top_sentences = {(k if k != "-1" else "菜喘"):v for k, v in sorted(self.data["per_sentence"].items(), key=lambda x: x[1], reverse=True)[:5]}
+        per_user_iter = self.data["per_user"].items()
+        return Stat(self.data["total"], len(self.data["per_user"]), top_sentences, max(per_user_iter, key=lambda x: x[1])[1])
